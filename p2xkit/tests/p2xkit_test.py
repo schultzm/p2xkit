293b86e5d7c744e910ea6cbe5f3b6b74f132a8d0
schultzm <dr.mark.schultz@gmail.com>
2020-04-09 20:42:07 +1000

    Added more test data

--- p2xkit/tests/p2xkit_test.py
+++ p2xkit/tests/p2xkit_test.py
@@ -7,7 +7,8 @@ import unittest
 from .. import (__parent_dir__,
                 __test_primers__,
                 __test_probes__,
-                __test_template__)
+                __test_template__,
+                __test_templates__)
 import pkg_resources
 from p2xkit.utils.psearcher import Psearcher#, Hit_parser
 
@@ -15,15 +16,21 @@ from p2xkit.utils.psearcher import Psearcher#, Hit_parser
 class BedTestCasePass(unittest.TestCase):
     maxDiff = None
     def setUp(self):
-        self.template = pkg_resources.resource_filename(__parent_dir__, __test_template__)
-        self.primers  = pkg_resources.resource_filename(__parent_dir__, __test_primers__)
-        self.probes   = pkg_resources.resource_filename(__parent_dir__, __test_probes__)
+        self.template = pkg_resources.resource_filename(__parent_dir__,
+                                                        __test_template__)
+        self.primers  = pkg_resources.resource_filename(__parent_dir__,
+                                                        __test_primers__)
+        self.probes   = pkg_resources.resource_filename(__parent_dir__,
+                                                        __test_probes__)
         self.mismatch = 20
+        self.templates = pkg_resources.resource_filename(__parent_dir__,
+                                                         __test_templates__)
 
     def psearcher(self):
         reaction = Psearcher(self.template, self.primers, self.mismatch)
         self.pcr_results = reaction.psearchit()
-        self.assertEqual(self.pcr_results.amplifiers['N_Sarbeco_DE'][0].hit_info,
+        self.assertEqual(self.pcr_results.amplifiers['N_Sarbeco_DE'][0]. \
+                         hit_info,
         """MN908947.3  
 	Severe acute respiratory syndrome coronavirus 2 isolate Wuhan-Hu-1, complete genome
 	CACATTGGCACCCGCAATC hits forward strand at 28706 with 0 mismatches
@@ -37,19 +44,24 @@ class BedTestCasePass(unittest.TestCase):
         #this is weird.
         # need to create a psearcher object,
         # but it's perhaps too much for a simple
-        # string parser.  Maybe break out the iupac def
+        # string parser.  Maybe break out the iupac def in Psearcher
         expanded = Psearcher(self.template,
                           self.primers,
                           self.mismatch)
-        collapsed = expanded.collapsed_iupac("CA[GAR]ATGTTAAA[GCS]ACACTATTAGCATA")
+        collapsed = expanded. \
+                    collapsed_iupac("CA[GAR]ATGTTAAA[GCS]ACACTATTAGCATA")
         self.assertEqual("CARATGTTAAASACACTATTAGCATA", collapsed)
 
     def amplifiers_parsed(self):
-        reaction = Psearcher(self.template,
+        reaction = Psearcher(self.templates,
                             self.primers,
                             self.mismatch)
-        pcr_results = reaction.psearchit()
-        for template_name, value in amplifiers.items():
-            print(type(value[0]))
+        pcr_results = reaction.psearchit() # a PrimerSearch.OutputRecord
+        # The attribute of interest in the output record is 'amplifiers'
+        # amplifiers is a dict.
+        # key is primerpairname
+        # value is an amplifier object [list].
+        for primerpair_name, amplifier in pcr_results.amplifiers.items():
+            print(type(primerpair_name pri))
 
         # print(amplifiers)
