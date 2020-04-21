beb560d3c3eeb78e65952464a6890d71cd06a359
schultzm <dr.mark.schultz@gmail.com>
2020-04-21 21:26:13 +1000

    About to try mapping all probes at once

--- p2xkit/utils/bowtier.py
+++ p2xkit/utils/bowtier.py
@@ -24,14 +24,39 @@ class Bowtier:
         self.bowtieindex = list(self.templates.parent.glob(f"{self.templates.stem}.*.bt2"))
 
     def bowtieit(self, amplimer_table, probes):
-        print(amplimer_table.to_csv(sep="\t"))
+        # print(amplimer_table.to_csv(sep="\t"))
+        map_results_dfs = [] 
         probes_dict = defaultdict(list)
         with open(probes, 'r') as input_handle:
             probes = list(SeqIO.parse(input_handle, 'fasta'))
             for probe in probes:
+                print(probe)
                 probes_dict[probe.id].append(probe)
         for primerpair_name, probes_list in probes_dict.items():
-            run_cmd = [f"bowtie2 -x {PurePath(self.templates.parent, self.templates.stem)} -U {tqmanprobe.seq} -c -a --end-to-end --very-sensitive" for tqmanprobe in probes_list]
-            print(run_cmd)
-        print()
-        # print(run_cmd)
+            # -L <int>           length of seed substrings; must be >3, <32 (22)
+            # -c                 <m1>, <m2>, <r> are sequences themselves, not files
+            # -U unpairedreads
+            # -a/--all           report all alignments; very slow, MAPQ not meaningful 
+            # -N <int>           max # mismatches in seed alignment; can be 0 or 1 (0)
+            # --np <int>         penalty for non-A/C/G/Ts in read/ref (1)
+            # -R <int>           for reads w/ repetitive seeds, try <int> sets of seeds (2)
+            map_cmds = [(f"bowtie2 -x {PurePath(self.templates.parent, self.templates.stem)} -U {tqmanprobe.seq} -c --all --end-to-end --very-sensitive -L 3 -N 1 --np 0 -R 10", tqmanprobe.description) for tqmanprobe in probes_list]
+            for map_cmd in map_cmds:
+                # map_cmd[0] is the bowtie2 cmd
+                # map_cmd[1] is the probe name
+                proc1 = Popen(shlex.split(map_cmd[0]), stdout=PIPE, stderr=PIPE)
+                samfile = proc1.stdout.fileno()
+                with pysam.AlignmentFile(samfile, "r") as sam:
+                    for rec in sam.fetch():
+                        if not rec.is_unmapped:
+
+                            # print(rec.to_dict())
+                            # if rec.flag == 16:
+                            #         seq_str = probe.seq.reverse_complement()
+        #                         id = f'probe_{idx}_rcomp_{str(primerpair)}'
+        #                     else:
+        #                         seq_str = probe.seq
+        #                         id = f'probe_{idx}_{str(primerpair)}'
+
+                    print()
+ 
