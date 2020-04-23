6daac0436dbd05e2d7e4e21246e5914b14fd4fc0
schultzm <dr.mark.schultz@gmail.com>
2020-04-23 12:45:50 +1000

    Just need to join the map results to the amplimer table

--- p2xkit/utils/bowtier.py
+++ p2xkit/utils/bowtier.py
@@ -29,13 +29,13 @@ class Bowtier:
         self.amplimer_table = amplimer_table
 
     def bowtieit(self):
-        print(self.amplimer_table)
+        # print(self.amplimer_table)
         probes_dict = defaultdict(list)
         with open(self.probes, 'r') as input_handle:
             probes = list(SeqIO.parse(input_handle, 'fasta'))
             for probe in probes:
-                probes_dict[probe.id].append(probe) # Need to specify that probe names must be same as primer_pair with a space and then a probe identifier (e.g., 'RdRP_SARSr_DE P2')
-        for rown in self.amplimer_table.index.values:
+                probes_dict[probe.id].append(probe) # Need to specify in readme that probe names must be same as primer_pair with a space and then a probe identifier (e.g., 'RdRP_SARSr_DE P2')
+        for rown in self.amplimer_table.index.values: #iterate through all the amplimers and map probes
             if pd.notnull(self.amplimer_table.loc[rown, 'amplicon_insert']):
                 subseq = SeqRecord(Seq(self.amplimer_table.loc[rown, 'amplicon_insert'],
                                        alphabet=IUPAC.ambiguous_dna),
@@ -43,50 +43,42 @@ class Bowtier:
                                 name=self.amplimer_table.loc[rown, 'template_name'],
                                 description=f"Amplimer_{self.amplimer_table.loc[rown, 'amplimer_n']:.0f}")
                 outhandle = Path(f"{subseq.id}_{subseq.description}.fasta")
-                SeqIO.write(subseq, outhandle, 'fasta') #more thought needs to be put into this
-                indexit(outhandle) # need to clean this up at end
-
-                map_results_dfs = [] 
-                for probe in probes_dict[subseq.id]:
-                    probe_id = probe.description.split(' ')[-1] #this is risky if space is not used to delimit probe ID
-                    print(probe_id)
-                    # Bowtie2 summary of options used
-                    # -L <int>           length of seed substrings; must be >3, <32 (22)
-                    # -c                 <m1>, <m2>, <r> are sequences themselves, not files
-                    # -U unpairedreads
-                    # -a/--all           report all alignments; very slow, MAPQ not meaningful # don't use this
-                    # -N <int>           max # mismatches in seed alignment; can be 0 or 1 (0)
-                    # --np <int>         penalty for non-A/C/G/Ts in read/ref (1)
-                    # -R <int>           for reads w/ repetitive seeds, try <int> sets of seeds (2)
-                    # -f                 query input files are (multi-)FASTA .fa/.mfa
-                    # --sam-no-qname-trunc Suppress standard behavior of truncating readname at first whitespace 
-                    #                      at the expense of generating non-standard SAM.
-                    map_cmd = f"bowtie2 -x {PurePath(outhandle.parent, outhandle.stem)} -U {probe.seq} -c --sam-no-qname-trunc --end-to-end  -L 7 -D 20"#q --np 0 -R 10"#, tqmanprobe.description) for tqmanprobe in probes_list]
+                SeqIO.write(subseq, outhandle, 'fasta') # writes out the amplicon insert to file
+                indexit(outhandle) # indexes the amplicon insert **need to clean this up at end
+                for probe in probes_dict[subseq.id]: #Iterate through probes for each primer pair
+                    map_cmd = f"bowtie2 -x {PurePath(outhandle.parent, outhandle.stem)} -U {probe.seq} -c --sam-no-qname-trunc --end-to-end  -L 7 -D 20"
                     proc1 = Popen(shlex.split(map_cmd), stdout=PIPE, stderr=PIPE)
                     samfile = proc1.stdout.fileno()
                     with pysam.AlignmentFile(samfile, "r") as sam:
-                        for rec in sam.fetch():
+                        map_results_dfs = [] # this will store the dfs of mappings
+                        for rec in sam.fetch(): # rec is a row in the SAM output
                             if not rec.is_unmapped:
-                                probe_range = {'probe_template_start': rec.get_aligned_pairs()[0][1],
-                                               'probe_template_end'  : rec.get_aligned_pairs()[-1][1]}
-                                keys_to_keep = ['name', 'flag', 'ref_name', 'ref_pos'] # SAM is 1-based, so ref_pos will be probe_template_start+1
+                                probe_range = {'probe_template_start': rec.get_aligned_pairs()[0][1], # these indices are 0-based
+                                               'probe_template_end'  : rec.get_aligned_pairs()[-1][1]} # 0-based
+                                keys_to_keep = ['flag', 'ref_name', 'ref_pos'] # SAM is **1-based, so ref_pos will be probe_template_start+1
                                 records = rec.to_dict()
                                 records_subset = {key: value for key, value in records.items() if key in keys_to_keep}
                                 records_subset = {**probe_range, **records_subset}
-                                records_subset['probe_length'] = len(probe.seq)
-                                records_subset['probe_length_aligned'] = records_subset['probe_template_end']-records_subset['probe_template_start']+1 #+1 as e.g., template matches at 21,22,23,24,25 are 5 matches but 25-21=4
-                                records_subset['probe_globally_aligned'] = ''.join(['True' if records_subset['probe_length_aligned']==records_subset['probe_length'] else 'False'])
-                                records_subset['probe_seq'] = str(probe.seq)
-                                records_subset['probe_orientation'] = 'FORWARD'
-                                records_subset['probe_match'] = subseq[records_subset['probe_template_start']:records_subset['probe_template_end']+1].seq
+                                records_subset['probe_name'] = probe.description
+                                records_subset['probe_id'] = probe.description.split(' ')[-1] #e.g., "P", "P1" or "P2"; this is risky if space is not used to delimit probe ID
                                 records_subset['primer_pair'] = probe.id
-                                if rec.flag == 16: # REVERSED
+                                records_subset['probe_length'] = len(probe.seq)
+                                records_subset['probe_seq'] = str(probe.seq)
+                                records_subset['probe_length_aligned'] = records_subset['probe_template_end']-records_subset['probe_template_start']+1 #+1 as e.g., number of template matches at 21,22,23,24,25 are 5 but 25-21=4
+                                records_subset['probe_globally_aligned'] = ''.join(['True' if records_subset['probe_length_aligned']==records_subset['probe_length'] else 'False'])
+                                records_subset['probe_orientation'] = 'FORWARD' # gets converted to reverse if samflag is 16, below
+                                records_subset['probe_match'] = subseq[records_subset['probe_template_start']:records_subset['probe_template_end']+1].seq #+1 as this is a slice index; this a Seq object
+                                if rec.flag == 16: # REVERSED, do some revcomp and flagging
                                     records_subset['probe_orientation'] = 'REVERSE'
                                     records_subset['probe_match'] = str(records_subset['probe_match'].reverse_complement()).upper()
-                                else:
+                                else: # convert match to string
                                     records_subset['probe_match'] = str(records_subset['probe_match']).upper()
                                 records_subset['probe_match_mismatch'] = _iupac_zipper(records_subset['probe_seq'], records_subset['probe_match'])
-                                sub_df = pd.DataFrame(records_subset, index=[records_subset['name']])
-                                print('SUBDF', sub_df.to_csv(sep="\t"))
-                                print('!!!', self.amplimer_table.loc[self.amplimer_table['primer_pair'] == records_subset['primer_pair']].to_csv(sep="\t"))
+                                sub_df = pd.DataFrame(records_subset, index=[records_subset['probe_name']])
+                                # print('SUBDF', sub_df.to_csv(sep="\t"))
+                                # print('!!!', self.amplimer_table.loc[self.amplimer_table['primer_pair'] == records_subset['primer_pair']].to_csv(sep="\t"))
                                 map_results_dfs.append(sub_df)
+                            else:
+                                print("UNMAPPED - Do something")
+                        df = pd.concat(map_results_dfs)
+                        print(df.to_csv(sep="\t"))
